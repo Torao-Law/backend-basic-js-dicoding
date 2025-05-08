ace98aebcd97b0b53a72b7d19f825a7959186329
torao-law <dandisyahrullah.12@gmail.com>
2025-05-08 20:51:08 +0700

    finish: rest api with api framework

--- node-js-rest-api/src/handler/handler.js
+++ node-js-rest-api/src/handler/handler.js
@@ -1,6 +1,29 @@
 const { notes } = require('./notes');
 const { nanoid } = require('nanoid');
 
+exports.findAllNoteHandler = () => ({
+  status: 'success',
+  data: notes,
+});
+
+exports.getNoteHandler = (req, res) => {
+  const { id } = req.params;
+
+  const note = notes.filter((item) => item.id === id)[0];
+
+  if (note) {
+    return {
+      status: 'success',
+      data: note
+    };
+  }
+
+  return res.response({
+    status: 'fail',
+    message: 'catatan tidak ditemukan'
+  });
+};
+
 exports.addNoteHandler = (req, res) => {
   const { title, tags, body } = req.payload;
 
@@ -40,4 +63,52 @@ exports.addNoteHandler = (req, res) => {
   });
   response.code(500);
   return response;
+};
+
+exports.updateNoteHandler = (req, res) => {
+  const { id } = req.params;
+  const { title, tags, body } = req.payload;
+
+  const index = notes.findIndex((note) => note.id === id);
+
+  if (index !== -1) {
+    const updatedAt = new Date().toISOString();
+
+    notes[index] = {
+      ...notes[index],
+      title,
+      tags,
+      body,
+      updatedAt,
+    };
+
+    return res.response({
+      status: 'success',
+      message: 'Catatan berhasil diperbarui',
+    });
+  }
+
+  return res.response({
+    status: 'fail',
+    message: 'Gagal memperbarui catatan. Id tidak ditemukan',
+  });
+};
+
+exports.deleteNoteHandler = (req, res) => {
+  const { id } = req.params;
+
+  const index = notes.findIndex((note) => note.id === id);
+
+  if (index !== -1) {
+    notes.splice(index, 1);
+    return res.response({
+      status: 'success',
+      message: 'Catatan berhasil dihapus',
+    });
+  }
+
+  return res.response({
+    status: 'fail',
+    message: 'Catatan gagal dihapus. Id tidak ditemukan',
+  });
 };
--- node-js-rest-api/src/routes/index.js
+++ node-js-rest-api/src/routes/index.js
@@ -2,9 +2,38 @@ const NoteHandler = require('../handler/handler');
 
 const routes = [
   {
-    method: 'POST',
+    method: 'GET',
+    path: '/',
+    handler: (req, res) => {
+      console.log(req);
+      console.log(res);
+      return 'hello world';
+    }
+  },
+  {
+    method: 'GET',
     path: '/notes',
+    handler: NoteHandler.findAllNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/note/{id}',
+    handler: NoteHandler.getNoteHandler,
+  },
+  {
+    method: 'POST',
+    path: '/note',
     handler: NoteHandler.addNoteHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/note/{id}',
+    handler: NoteHandler.updateNoteHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/note/{id}',
+    handler: NoteHandler.deleteNoteHandler,
   }
 ];
 
